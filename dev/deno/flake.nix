e67501d3910f31674e6621817351a010f7e674b8
Divy Srivastava <me@littledivy.com>
2025-09-23 08:49:07 +0530

    add deno development shell

---
+++ dev/deno/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Deno dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          llvmPackages_20.clang
+          lld_20
+          llvmPackages_20.libllvm
+          rustup
+          pkg-config
+          cmake
+        ];
+      };
+    });
+}
