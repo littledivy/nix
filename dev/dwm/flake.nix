18a409e320de4e8ff582b1f3ce636f7abcce23a4
Divy Srivastava <me@littledivy.com>
2025-09-24 09:48:52 +0530

    add dwm develop flake

---
+++ dev/dwm/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "dwm dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gnumake
+            xorg.libX11.dev
+            xorg.libXft
+            xorg.libXinerama
+          ];
+        };
+      }
+    );
+}
