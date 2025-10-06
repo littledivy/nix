948d075c5dfe7c6d5344c593c9efefc18d96cac5
Divy Srivastava <me@littledivy.com>
2025-10-06 17:22:06 +0530

    remove apple-music-rpc

--- home.nix
+++ home.nix
@@ -12,16 +12,6 @@ let
     sha256 = "sha256-OIMKlO1mtY0Y5x0RudsXYr2CLJSaBOoi/lTbtnyO4sk=";
   };
 
-  apple-music-rpc =
-    if pkgs.stdenv.isDarwin then
-      pkgs.fetchFromGitHub {
-        owner = "NextFire";
-        repo = "apple-music-discord-rpc";
-        rev = "aa56ed5";
-        sha256 = "sha256-NCUXuHxQI3hHpnuEVn7+QHNVXK7C7RPa/x7WCNz3Ut8=";
-      }
-    else
-      null;
   prr = pkgs.prr.overrideAttrs {
     src = pkgs.fetchFromGitHub {
       owner = "danobi";
@@ -131,31 +121,6 @@ in
       foreground = "#bcbcbc";
     };
   };
-
-  launchd.agents.apple-music-rpc =
-    if pkgs.stdenv.isDarwin then
-      {
-        enable = true;
-        config = {
-          Label = "moe.yuru.music-rpc";
-          ProgramArguments = [
-            "sh"
-            "-c"
-            "cd '${config.home.homeDirectory}/Library/Caches/' && exec ${pkgs.deno}/bin/deno run --unstable-kv --allow-write --allow-import --allow-env --allow-net --allow-run --allow-read ${apple-music-rpc}/music-rpc.ts"
-          ];
-          WorkingDirectory = "${apple-music-rpc}";
-          EnvironmentVariables = {
-            PATH = "${pkgs.deno}/bin:/usr/bin";
-          };
-          RunAtLoad = true;
-          KeepAlive = true;
-
-          StandardOutPath = "${config.home.homeDirectory}/Library/Logs/apple-music-rpc.out.log";
-          StandardErrorPath = "${config.home.homeDirectory}/Library/Logs/apple-music-rpc.err.log";
-        };
-      }
-    else
-      null;
 
   home.file.".vimrc".source = "${vimrc}/.vimrc";
   home.file.".vimrc".force = true;
