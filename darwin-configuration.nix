e95bd6e3ea40fe971ae18c1b4d58c703a2b79fa2
Divy Srivastava <me@littledivy.com>
2025-12-19 18:38:09 +0530

    darwin: add postgres

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -12,7 +12,7 @@ in
   system.build.applications = lib.mkForce (
     pkgs.buildEnv {
       name = "system-applications";
-      pathsToLink = ["/Applications"];
+      pathsToLink = [ "/Applications" ];
       paths =
         config.environment.systemPackages
         ++ (lib.concatMap (x: x.home.packages) (lib.attrsets.attrValues config.home-manager.users));
@@ -54,7 +54,42 @@ in
     };
   };
 
- system.activationScripts.extraActivation.text = ''
+  system.activationScripts.preActivation = {
+    enable = true;
+    # (note: the DB runs as my user! `staff` is the default non-admin group)
+    text =
+      let
+        dataDir = config.services.postgresql.dataDir;
+      in
+      ''
+        if [ ! -d "${dataDir}" ]; then
+          echo "creating PostgreSQL data directory..."
+          sudo mkdir -m 750 -p "${dataDir}"
+          chown -R ${config.system.primaryUser}:staff "${dataDir}"
+        fi
+      '';
+  };
+
+  services.postgresql.initdbArgs = [
+    # We should really change the (internal) `superUser` option instead 🤔
+    "-U ${config.system.primaryUser}"
+  ];
+
+  launchd.user.agents.postgresql.serviceConfig = {
+    StandardErrorPath = "/tmp/postgres.error.log";
+    StandardOutPath = "/tmp/postgres.log";
+  };
+  services.postgresql = {
+    enable = true;
+    enableTCPIP = true;
+    authentication = pkgs.lib.mkOverride 10 ''
+            #type database  DBuser  origin-address auth-method
+            local all       all     trust
+      			host  all       all     all  trust
+    '';
+  };
+
+  system.activationScripts.extraActivation.text = ''
     softwareupdate --install-rosetta --agree-to-license
   '';
 
